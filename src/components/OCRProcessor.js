a0bd98cf6b4e9843f311139293f9ba0fa603b25c
Negakyo <thomas.crqr@gmail.com>
2025-01-15 14:57:22 +0100

    feat(regex)

--- src/components/OCRProcessor.js
+++ src/components/OCRProcessor.js
@@ -19,24 +19,35 @@ const OCRProcessor = () => {
         }
       );
 
-      const regex = /(M.*TP)/;
+      const regex = /(M.*T[PD]|SIDOBRE|ALBINQUE)/;
       const match = text.match(regex);
 
       if (match && match[1]) {
-        let transformedText = match[1]
-          .replace(/o|O/g, '0')
-          .replace(/\s/g, '');
+        let transformedText = match[1];
 
-        const formatCheck = /M\d{2}/;
-        if (!formatCheck.test(transformedText)) {
-          setFilteredText('');
-        } else {
-          setFilteredText(transformedText);
-          setTextFinale(transformedText);
+        if (/M.*T[PD]/.test(match[1])) {
+          transformedText = transformedText
+            .replace(/o|O/g, '0')
+            .replace(/\s/g, '');
+
+          const formatCheck = /M\d{2}/;
+          if (!formatCheck.test(transformedText)) {
+            setFilteredText('');
+            return;
+          }
+        } else if (/SIDOBRE|ALBINQUE/.test(match[1])) {
+          const formatCheck = /^SIDOBRE$|^ALBINQUE$/;
+          if (!formatCheck.test(transformedText)) {
+            setFilteredText('');
+            return;
+          }
         }
+        setFilteredText(transformedText);
+        setTextFinale(transformedText);
       } else {
         setFilteredText('');
       }
+
     } catch (err) {
       console.error('Erreur :', err);
     } finally {
